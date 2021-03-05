74535f792be982bbfce631abf5fd5ce9b0039d36
Curtis <Curtishollidge@gmail.com>
2021-03-05 16:06:29 +0000

    New changes(Not sure what is here but dont want to lose anything).

--- Assets/HexScene/Script/Player Scrip/Classes/Pyromancer/Fireabilities.cs
+++ Assets/HexScene/Script/Player Scrip/Classes/Pyromancer/Fireabilities.cs
@@ -4,28 +4,32 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
-[CreateAssetMenu (fileName = "FireAbilities" , menuName ="AbilitySystem/Abilites/FireAbilities")]
+[CreateAssetMenu(fileName = "FireAbilities", menuName = "AbilitySystem/Abilites/FireAbilities")]
 public class Fireabilities : Abilities
 {
-    public void Awake()
-    {
-        type = abilityType.Fire;
-
-    }
+    
+    List<SpellEffects> effects;
     //How far the player will be sent
-    [SerializeField] private float kockback;
+    [SerializeField] private float knockback;
     //How much % the Player will recieve
     [SerializeField] private float dot;
     //How much the damage the player will take
     [SerializeField] private float damage;
+    [SerializeField] private string status;
 
     //These are getters
-    public float Kockback => kockback;
+    public float KnockBack => knockback;
     public float DOT => dot;
-    public float Damange => damage;
+    public float Damage => damage;
 
-    virtual public void Execute() {
+    public string Status => status;
 
+    virtual public void Execute(GameObject go, GameObject player){
+
+        foreach (SpellEffects effect in effects)
+        {
+            effect.ExceuteEffect(this,go,player);
+        }
     }
 
 }
